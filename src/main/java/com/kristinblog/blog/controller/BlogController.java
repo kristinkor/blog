f55f1b5082d71af460a774bbb4364e01d9f33e8e
korzhenevskaya <kristinkor@gmail.com>
2023-07-01 23:46:03 -0400

    added article details page

--- src/main/java/com/kristinblog/blog/controller/BlogController.java
+++ src/main/java/com/kristinblog/blog/controller/BlogController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -27,11 +32,22 @@ public class BlogController {
         return "blog-add";
     }
 
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value="id") long articleId, Model model){
+        Optional<Article> article = articleRepository.findById(articleId);
+        ArrayList <Article> result = new ArrayList<>();
+        article.ifPresent(result :: add);
+        model.addAttribute("article", result);
+        return "blog-details";
+    }
+
     @PostMapping("/blog/add")
     public String blogPostAdd(@RequestPart String title, @RequestPart String anons, @RequestPart String full_text, Model model){
         Article post = new Article(title, anons, full_text);
         articleRepository.save(post);
         return "redirect:/blog";
     }
+
+
 
 }
